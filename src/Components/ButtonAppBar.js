92664d96677dccc25830359e8d1ea60b70441a15
Orestis.Mark <orestis.markozanes@gmail.com>
2019-03-16 12:40:50 -0400

    nav bar

---
+++ src/Components/ButtonAppBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Button variant="h6" color="inherit" className={classes.grow}>
+            Profile
+          </Button>
+          <Button variant="h6" color="inherit" className={classes.grow}>
+            Form
+          </Button>
+          <Button variant="h6" color="inherit" className={classes.grow}>
+            Visualization
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
