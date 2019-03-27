f55486a33bc5bf13f8a31074fc662f0793cf1d62
Kelly Chen <kell31ch@gmail.com>
2019-03-27 14:34:53 -0400

    profile component completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import ButtonAppBar from './Components/ButtonAppBar';
+import Profile from './Components/Profile';
+import Dashboard from './Components/Dashboard';
 
 class App extends Component {
   render() {
     return (
-      <ButtonAppBar></ButtonAppBar>  
+      <Profile></Profile>
     );
   }
 }
